95ef83d0bbcb5ce38e7829cd94c9d1f402b279d9
NeutronSun <lorenzosanseverino2003@gmail.com>
2021-11-05 19:45:27 +0100

    1

--- AlgRsa/src/Main.java
+++ AlgRsa/src/Main.java
@@ -6,31 +6,56 @@ public class Main {
         TheBigFile fl = new TheBigFile();
         Fermat f = new Fermat(fl);
         Scanner in = new Scanner(System.in);
-        if(!fl.fileExists()){
-            System.out.println("Unable to find the file with the prime numbers for the RSA algorithm, please create it.");
+        if (!fl.fileExists()) {
+            System.out
+                    .println("Unable to find the file with the prime numbers for the RSA algorithm, please create it.");
             f.findPrimeWith();
         }
         Rsa rsa = new Rsa();
-        while(true) {
+        while (true) {
             System.out.println("1| Create a new File");
             System.out.println("2| Do RSA encryption");
-            switch(in.nextInt()) {
+            switch (in.nextInt()) {
 
-                case 1:
+            case 1:
                 f.findPrimeWith();
                 break;
 
-                case 2:
+            case 2:
                 System.out.print("Enter the message:");
                 in.nextLine();
                 String msg = in.nextLine();
-                
-               BigInteger chiper =  rsa.encrypt(msg);
-               System.out.println(chiper);
-               System.out.println(rsa.decrypt(chiper));
-                //System.out.println("the decrypted message is: " + rsa.decrypt(cryp));
+
+                BigInteger chiper = rsa.encrypt(msg);
+                System.out.println(chiper);
+                System.out.println(rsa.decrypt(chiper));
+                // System.out.println("the decrypted message is: " + rsa.decrypt(cryp));
             }
         }
-        
+
     }
+
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 }
--- AlgRsa/src/Rsa.java
+++ AlgRsa/src/Rsa.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
-public class Rsa{
+public class Rsa {
     private static BigInteger lastPrime = new BigInteger("150000000000000000000000000000000000000000000000973");
     private BigInteger p = new BigInteger("47");
     private BigInteger q = new BigInteger("71");
@@ -10,86 +10,83 @@ public class Rsa{
     private BigInteger e = new BigInteger("0");
     private BigInteger d = new BigInteger("0");
 
-    {lastPrime = newPrime(lastPrime);}
+    {
+        lastPrime = newPrime(lastPrime);
+    }
 
-    public Rsa(){
+    public Rsa() {
         p = newPrime(lastPrime);
-        System.out.println("setted");
         q = newPrime(lastPrime);
-        System.out.println("setted");
+        ;
         n = p.multiply(q);
         System.out.println("setted");
         e = coprimes();
-        System.out.println("setted");
-        //d = findD();
+        // d = findD();
         BigInteger phi = phi();
         d = e.modInverse(phi);
         System.out.println("phi(n): " + phi() + "|q: " + q + "|p: " + p + "|n: " + n + "|e: " + e + "|d: " + d);
     }
-    public BigInteger findD(){
+
+    public BigInteger findD() {
         BigInteger k = BigInteger.TEN.pow(e.toString().length());
         BigInteger phi = phi();
-        while(true) {
-            if(k.multiply(e).mod(phi).equals(BigInteger.ONE))
+        while (true) {
+            if (k.multiply(e).mod(phi).equals(BigInteger.ONE))
                 return k;
             k = k.add(BigInteger.ONE);
         }
-    } 
-    public BigInteger phi(){
+    }
+
+    public BigInteger phi() {
         return p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
     }
 
     /*
-    public BigInteger mcm(BigInteger a, BigInteger b){
-        if (b.equals(BigInteger.ZERO)) return a;
-        return mcm(b,a.mod(b));
-    }
-    */
+     * public BigInteger mcm(BigInteger a, BigInteger b){ if
+     * (b.equals(BigInteger.ZERO)) return a; return mcm(b,a.mod(b)); }
+     */
 
-    public BigInteger coprimes(){
+    public BigInteger coprimes() {
         BigInteger prm = q;
         return newPrime(prm);
     }
-    public BigInteger newPrime(BigInteger prime){
+
+    public BigInteger newPrime(BigInteger prime) {
         BigInteger a = new BigInteger("2");
-        do{
+        do {
             prime = prime.add(BigInteger.TWO);
-        }while(!a.modPow(prime.subtract(BigInteger.ONE), prime).equals(BigInteger.ONE));
+        } while (!a.modPow(prime.subtract(BigInteger.ONE), prime).equals(BigInteger.ONE));
         lastPrime = prime;
         return prime;
     }
 
-    public String getD(){
+    public String getD() {
         return d.toString();
     }
 
-    public String getN(){
+    public String getN() {
         return n.toString();
     }
 
-    public BigInteger encrypt(String ss) throws UnsupportedEncodingException{
-        System.out.println(ss.length());
+    public BigInteger encrypt(String ss) throws UnsupportedEncodingException {
         ss = ss.replaceAll("<3", new StringBuilder().appendCodePoint(0x1F497).toString());
         byte[] erbite = ss.getBytes("UTF-8");
-        for(int i = 0; i < erbite.length; i++){
-            System.out.println("byte: " +i+ " " + erbite[i]);
-        }
-        BigInteger plainText = new BigInteger(erbite);
-        System.out.println("non criptato: " + plainText);
-        return plainText.modPow(e,n);
+        System.out.println(new String(erbite));
+        BigInteger plainText = new BigInteger(erbite); // <n
+        if (plainText.min(n).equals(plainText))
+            System.out.println("1: ok bro");
+        return plainText.modPow(e, n); // still <n
     }
 
-    public String decrypt(BigInteger msg){
-        msg = msg.modPow(d,n);
+    public String decrypt(BigInteger msg) {
+        if (msg.min(n).equals(msg))
+            System.out.println("2:ok bro");
+        msg = msg.modPow(d, n);
         byte[] erbite = msg.toByteArray();
-        for(int i = 0; i < erbite.length; i++){
-            System.out.println("byte: " +i+ " " + erbite[i]);
-        }
         String ss = new String(erbite);
-       // System.out.println("msg^n: " + msg.pow(Integer.valueOf(d.toString())));
+        // System.out.println("msg^n: " + msg.pow(Integer.valueOf(d.toString())));
         return ss;
 
     }
-
 
 }
