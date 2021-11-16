ce24d613c771488a025b88ab55cec484154831de
NeutronSun <lorenzosanseverino2003@gmail.com>
2021-11-16 11:12:33 +0100

    finished

--- AlgRsa/src/Main.java
+++ AlgRsa/src/Main.java
@@ -3,30 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TheBigFile fl = new TheBigFile();
-        Fermat f = new Fermat(fl);
         Scanner in = new Scanner(System.in);
-        if (!fl.fileExists()) {
-            System.out
-                    .println("Unable to find the file with the prime numbers for the RSA algorithm, please create it.");
-            f.findPrimeWith();
-        }
         Rsa rsa = new Rsa();
+        String msg = "";
+        String chiper = "";
         while (true) {
-            System.out.println("1| Create a new File");
+            System.out.println("1| Encrypt");
             System.out.println("2| Do RSA encryption");
             switch (in.nextInt()) {
 
             case 1:
-                f.findPrimeWith();
+                System.out.print("Enter the message:");
+                msg = rsa.encrypt(in.next());
+                System.out.println("The encrypted message is: " + msg);
                 break;
 
             case 2:
                 System.out.print("Enter the message:");
-                in.nextLine();
-                String msg = in.nextLine();
-                msg = rsa.encrypt(msg);
-                System.out.println("-------------------------\nthe decrypted message is: " + rsa.decrypt(msg));
+                System.out.println("the decrypted message is: " + rsa.decrypt(in.next()));
             }
         }
 
